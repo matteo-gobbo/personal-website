9db1cd3ee96ff786576df5354e14a6b45c21bcce
Matteo Gobbo <info@matteogobbo.it>
2025-12-28 01:27:29 +0100

    feat: improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://matteogobbo.it",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
   server: {
     port: 3000,
   },
