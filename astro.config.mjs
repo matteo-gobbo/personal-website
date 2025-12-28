b93ae7c1400ef2dbd3c81c2109142d2df2406262
Matteo Gobbo <info@matteogobbo.it>
2025-12-28 15:49:28 +0100

    chore: remove tailwindcss from the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://matteogobbo.it",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-  ],
-  server: {
-    port: 3000,
-  },
+  integrations: [icon()],
 });
